8844c417778c837509b7d4e1390c302ae4d4b6ee
hongjungeun <gjhong1129@gmail.com>
2022-09-29 23:07:50 +0900

    fix(~/go/src/github.com/fineroot1253/tistoryAPI/model): category 데이터 모델 타입 수정
    ## category 데이터 모델 타입 수정
     - category
     	- CategoryResult 롤백
    	- CategoryItem의 Categories 데이터 타입 수정

--- model/category.go
+++ model/category.go
@@ -1,14 +1,14 @@
 package model
 
 type CategoryResult struct {
-	Status string         `json:"status"`
-	Item   []CategoryItem `json:"item"`
+	Status string       `json:"status"`
+	Item   CategoryItem `json:"item"`
 }
 
 type CategoryItem struct {
-	Url          string       `json:"url"`
-	SecondaryUrl string       `json:"secondaryUrl"`
-	Categories   CategoryData `json:"categories"`
+	Url          string         `json:"url"`
+	SecondaryUrl string         `json:"secondaryUrl"`
+	Categories   []CategoryData `json:"categories"`
 }
 
 type CategoryData struct {
